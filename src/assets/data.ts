8f6b1021b0a5a6517e2956fb025039b4cf8217a0
riyaa345 <riyaannreji12@gmail.com>
2023-03-03 09:12:36 +0530

    first commit

---
+++ src/assets/data.ts
@@ -0,0 +1,61 @@
+export const data = [
+    {
+        title:"KASARGOD",
+        image:"i1.jpg",
+        string: "Kasaragod District is one of the 14 districts in the state of Kerala, India. Kasaragod became part of Kannur district following the reorganisation of states and formation of Kerala in November 1, 1956. Kasaragod was declared as a district on 24 May 1984."
+        
+    },
+    {
+        title:"KANNUR",
+        image:"i2.jpeg",
+        string:"Kannur District is one of the 14 districts along the west coast in the state of Kerala, India. The town of Kannur is the district headquarters and gives the district its name. The old name, Cannanore is the anglicised form of the Malayalam name Kannur. Kannur district is bounded by Kasaragod District to the north, Kozhikode district to the south and Wayanad District to the southeast."
+    },{
+        title:"WAYANAD",
+        image:"i3.jpg",
+        string:"Wayanad district is the 12th District in the state of Kerala in India.Mananthavady, Sulthanbathery, and Vythiri Taluks are all in Wayanad.[1] The name Wayanad comes from Vayal Nadu and it means the land of paddy fields. It is a high, flat land between 700 meters and 2100 meters above sea level. It is in northeaster Kerala, in the Western Ghat mountains. Wayandad is one of the biggest foreign exchange earners in Kerala because it makes money-earning crops like pepper, cardamom, coffee, tea, and other spices"
+    },{
+        title:"KOZHIKODE",
+        image:"i4.jpg",
+        string:"Kozhikode, also called Calicut, city, northern Kerala state, southwestern India. It is situated on the Malabar Coast, 414 miles (666 km) west-southwest of Chennai (Madras) by rail. Kozhikode was once a famous cotton-weaving centre, and it is remembered as the place of origin of calico, to which it gave its name"
+    },{
+        title:"MALAPPURAM",
+        image:"i5.jpg",
+        string:"Malappuram district is a district in the state of Kerala, India. The headquarters at Malappuram. The district was formed on 16 June 1969. Malappuram district is composed of portions of the former Palakkad and Kozhikode districts: Ernad taluk and portions of Tirur taluk in Kozhikode district, and portions of Perinthalmannna and Ponnani taluks in Palakkad district."
+    },{
+        title:"PALAKKAD",
+        image:"i6.jpg",
+        string:"Palakkad District is one of the 14 districts of the Indian state of Kerala. Also, it is the largest district in kerala from 2006. The city of Palakkad is the district headquarters. Palakkad is bordered on the northwest by the Malappuram District, on the southwest by the Thrissur District, on the northeast by The Nilgiris District and on the east by Coimbatore district of Tamil Nadu. The district is 24.4% urbanised according to the census of 2011. The district is nicknamed the granary of Kerala and Rice bowl of Kerala"
+    },{
+        title:"THRISSUR",
+        image:"i7.jpg",
+        string:"Thrissur (also Trichur, Trissur) is a revenue district of Kerala in the central part of that state. Spanning an area of about 3,032 km2, Thrissur district is home to over 10% of Kerala’s population. Thrissur district was formed on July 1, 1949, with the headquarters at Thrissur City. Thrissur is known as the cultural capital of Kerala, and the land of Poorams. The district is famous for its ancient temples, churches, and mosques. Thrissur Pooram is the most colourful and spectacular temple festival in Kerala. The district was formed on July 1, 1949."
+    },{
+        title:"ERNAKULAM",
+        image:"i8.jpg",
+        string:"Ernakulam is a district of Kerala, India in the central part of that state. Spanning an area of about 3,068 km2, Ernakulam district is home to over 12% of Kerala’s population. Its headquarters is located at Kakkanad, a suburb of Kochi city. Ernakulam is known as the commercial capital of Kerala. The district is famous for its ancient temples, churches, and mosques. The district includes the largest metropolitan region of the state, Greater Cochin. Ernakulam district is the highest revenue yielding district in the state"
+    },{
+        title:"IDUKKKI",
+        image:"i9.jpeg",
+        string:"Idukki is one of the 14 districts of Kerala state, India,created on 26 January 1972. At that time, the district headquarters was at Kottayam. In June 1976 it was moved to Painavu. Idukki which lies in the Western Ghats of Kerala. Idukki is the second largest district in area but has the lowest population density. Idukki has a vast forest reserve area; more than a half of the district is covered by forests. The urban areas are densely populated whereas villages are sparsely populated. Idukki is also known as the spice garden of Kerala."
+    },{
+        title:"KOTTAYAM",
+        image:"i10.jpeg",
+        string:"Kottayam District is one of the 14 districts in the state of Kerala, India. Though the district is 157 km south to Kunnamkulam, the center of Kerala. It is located in an area with biggest diameter and population. It is the only district having no border with either the Western Ghats or Arabian Sea. The Kerala government and Central govt both agreed to built an airport near Sabarimala."
+    },{
+        title:"ALAPPUZHA",
+        image:"i11.jpeg",
+        string:"Alappuzha is one of the 14 districts in the state of Kerala in India. It was formed as Alleppey District on August 17, 1957. The name of the district was officially changed to Alappuzha in 1990.Alappuzha is well connected by waterways to various other parts of Kerala, including the famous tourist destination of Kumarakom; and the district is itself a well known tourist destination. The district is known for its Coir factories, as most of Kerala's coir industries are in and around Alappuzha.The district is also known for its communist traditions. "
+    },{
+        title:"PATHANAMTHITTA",
+        image:"i12.jpeg",
+        string:"Pathanamthitta District is a district in the southern part of Kerala, India. The district headquarters is in the town of Pathanamthitta. There are four municipalities in Pathanamthitta: Tiruvalla, Adoor, Pathanamthitta and Pandalam. According to the 2001 Census of India, the population was 1,231,577,making it the third least populous district in Kerala (out of 14), after Wayanad and Idukki. Pathanamthitta has been declared the first polio-free district in India"
+    },{
+        title:"KOLLAM",
+        image:"i13.jpg",
+        string:"Kollam district (formerly Quilon) is one of 14 districts of the state of Kerala, India. The district has a cross-section of Kerala's natural attributes; it is endowed with a long coastline, a major Laccadive Sea seaport and an inland lake (Ashtamudi Lake). The district has many source of waterbodies. Kallada river is one among it and the east side land of river is EastKallada and the west side land is WestKallada. Kallada Boat race is one among the famous festival events of district. Even though it is a competition between two land sides of river many boat clubs from various places even beyond the district use to participate in the event. Kollam is the capital of Kerala's cashew industry"
+    },{
+        title:"THIRUVANANTHAPURAM",
+        image:"i14.jpeg",
+        string:"Thiruvananthapuram District is the southernmost district of the coastal state of Kerala. It is the largest city in Kerala. It came into existence in the year 1957. The headquarters is the city of Thiruvananthapuram (Trivandrum) which is also the capital city of Kerala. The district has an area of 2,192 square kilometres (846 sq mi) and a population of 3,307,284 (as per the 2011 census), the second-most populous district in Kerala after Malappuram district"
+    }
+]
